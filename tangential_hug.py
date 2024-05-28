2b5fd492c20d12f01f42887477bad968ed729596
Mauro Camara Escudero <maurocamaraescudero@gmail.com>
2024-05-28 18:36:08 +0100

    more figures

--- tangential_hug.py
+++ tangential_hug.py
@@ -40,3 +40,45 @@ def thug(x0, step_size, B, N, alpha, log_dens, grad_f, rng=None):
             x0 = x
         samples.append(x0)
     return np.vstack(samples), acceptances.mean(), esjd, time.time() - start_time
+
+
+def thug_and_snug(x0, step_thug, step_snug, p_thug, B, N, alpha, log_dens, grad_f, rng=None):
+    """
+    Tangential Hug. Notice that it doesn't matter whether we use the gradient of pi or
+    grad log pi to tilt the velocity.
+    """
+    start_time = time.time()
+    # Grab dimension, initialize storage for samples & acceptances
+    rng = np.random.default_rng(seed=np.random.randint(low=0, high=10000)) if rng is None else rng
+    samples = []
+    acceptances = np.zeros(N)
+    esjd = 0.0  # rao-blackwellised
+    signs = rng.choice(a=[1.0, -1.0], size=N, replace=True, p=[p_thug, 1-p_thug])
+    for i in range(N):
+        step_size = step_thug if signs[i] == 1 else step_snug
+        v0s = rng.normal(loc=0.0, scale=1.0, size=x0.shape)  # Draw velocity spherically
+        g = grad_f(x0)              # Compute gradient at x0
+        g = g / np.linalg.norm(g)                  # Normalize
+        v0 = v0s - alpha * g * (g @ v0s)  # Tilt velocity
+        v, x = v0, x0                    # Housekeeping
+        logu = np.log(rng.uniform())            # Acceptance ratio
+
+        for _ in range(B):
+            x = x + step_size*v/2           # Move to midpoint
+            g = grad_f(x)          # Compute gradient at midpoint
+            ghat = g / np.linalg.norm(g)          # Normalize
+            v = signs[i]*(v - 2*(v @ ghat) * ghat)  # Reflect velocity using midpoint gradient
+            x = x + step_size*v/2           # Move from midpoint to end-point
+        # Unsqueeze the velocity
+        g = grad_f(x)
+        g = g / np.linalg.norm(g)
+        v = v + (alpha / (1 - alpha)) * g * (g @ v)
+        # In the acceptance ratio must use spherical velocities, hence v0s and the unsqueezed v
+        log_ar = log_dens(x) - 0.5*(v@v) - log_dens(x0) + 0.5*(v0s@v0s)
+        ap = np.exp(np.clip(log_ar, a_min=None, a_max=0.0))  # acceptance probability
+        esjd += ap*np.linalg.norm(x0 - x)**2
+        if logu <= log_ar:
+            acceptances[i] = 1         # Accepted!
+            x0 = x
+        samples.append(x0)
+    return np.vstack(samples), acceptances.mean(), esjd, time.time() - start_time
--- visualize_samples.py
+++ visualize_samples.py
@@ -2,9 +2,10 @@ import pickle
 import numpy as np
 from matplotlib import rc
 import matplotlib.pyplot as plt
-from tangential_hug import thug
+from tangential_hug import thug, thug_and_snug
 from hamiltonian_monte_carlo import hmc
 from bip import find_point_on_theta_manifold, log_post, grad_forward, constraint, grad_neg_log_post
+import tensorflow_probability as tfp
 
 
 if __name__ == "__main__":
@@ -13,7 +14,7 @@ if __name__ == "__main__":
     B = 20
     N = 2500
     seed = 1234
-    sigma = 0.001
+    sigma = 1.0
     rng = np.random.default_rng(seed=seed)
 
     # Initial point
@@ -23,11 +24,28 @@ if __name__ == "__main__":
     s_thug, ap_thug, esjd_thug, runtime_thug = thug(
         x0=theta, step_size=step, B=B, N=N, alpha=0.0,
         log_dens=lambda t: log_post(t, sigma=sigma), grad_f=grad_forward, rng=rng)
+    ess_thug = min(tfp.mcmc.effective_sample_size(s_thug))
     # HMC samples
     _ = np.seterr(invalid='ignore', over='ignore')
     s_hmc, ap_hmc, esjd_hmc, runtime_hmc = hmc(
             x0=theta, L=B, step=step, N=N, log_dens=lambda t: log_post(t, sigma=sigma),
             gnld=lambda t: grad_neg_log_post(t, sigma), rng=rng)
+    ess_hmc = min(tfp.mcmc.effective_sample_size(s_hmc))
+    # THUG + SNUG, PROB(THUG) = 0.1
+    s_ts01, ap_ts01, esjd_ts01, runtime_ts01 = thug_and_snug(
+        x0=theta, step_thug=step, step_snug=step, p_thug=0.1, B=B, N=N, alpha=0.0,
+        log_dens=lambda t: log_post(t, sigma=sigma), grad_f=grad_forward, rng=rng)
+    ess_ts01 = min(tfp.mcmc.effective_sample_size(s_ts01))
+    # THUG + SNUG, PROB(THUG) = 0.5
+    s_ts05, ap_ts05, esjd_ts05, runtime_ts05 = thug_and_snug(
+        x0=theta, step_thug=step, step_snug=step, p_thug=0.5, B=B, N=N, alpha=0.0,
+        log_dens=lambda t: log_post(t, sigma=sigma), grad_f=grad_forward, rng=rng)
+    ess_ts05 = min(tfp.mcmc.effective_sample_size(s_ts05))
+    # THUG + SNUG, PROB(THUG) = 0.9
+    s_ts08, ap_ts08, esjd_ts08, runtime_ts08 = thug_and_snug(
+        x0=theta, step_thug=step, step_snug=step, p_thug=0.8, B=B, N=N, alpha=0.0,
+        log_dens=lambda t: log_post(t, sigma=sigma), grad_f=grad_forward, rng=rng)
+    ess_ts08 = min(tfp.mcmc.effective_sample_size(s_ts08))
 
     # Density grid
     xx, yy = np.linspace(-2, 2, 100), np.linspace(-2, 2, 100)
@@ -37,20 +55,31 @@ if __name__ == "__main__":
 
     # Plot samples
     rc('font', **{'family': 'STIXGeneral'})
-    fig, ax = plt.subplots(figsize=(5, 5))
+    fig, ax = plt.subplots(figsize=(20, 4), ncols=5)
     n_std = 15
+    ss = 12
+    titles = ["THUG", "HMC", "0.1 THUG, 0.9 SNUG", "0.5 THUG, 0.5 SNUG", "0.8 THUG, 0.2 SNUG"]
     levels = sigma * np.arange(-n_std, n_std+1)
-    contours = ax.contourf(XX, YY, ZZ, levels=levels, cmap='viridis')
-    ax.contour(XX, YY, ZZ, levels=[0.0], colors='black')
-    ax.scatter(*s_thug.T, s=4, color='dodgerblue', ec='navy', label='THUG')
-    ax.scatter(*s_hmc.T, s=4, color='lightcoral', ec='brown', label='HMC')
-    ax.set_aspect('equal')
-    ax.set_xlabel(r"$\mathregular{\theta_0}$", fontsize=15)
-    ax.set_ylabel(r"$\mathregular{\theta_1}$", fontsize=15)
-    plt.colorbar(contours, label="Standard deviations from " + r"$\mathregular{C(\theta) = F(\theta) - y = 0}$")
+    for i in range(5):
+        contours = ax[i].contourf(XX, YY, ZZ, levels=levels, cmap='viridis')
+        if i == 5:
+            ax[-1].set_colorbar(contours,
+                                label="Standard deviations from " + r"$\mathregular{C(\theta) = F(\theta) - y = 0}$")
+        ax[i].contour(XX, YY, ZZ, levels=[0.0], colors='black')
+    ax[0].scatter(*s_thug.T, s=ss, color='dodgerblue', ec='navy', label=f"{ess_thug:.2f}")
+    ax[1].scatter(*s_hmc.T, s=ss, color='lightcoral', ec='brown', label=f"{ess_hmc:.2f}")
+    ax[2].scatter(*s_ts01.T, s=ss, color='lawngreen', ec='olivedrab', label=f"{ess_ts01:.2f}")
+    ax[3].scatter(*s_ts05.T, s=ss, color='thistle', ec='purple', label=f"{ess_ts05:.2f}")
+    ax[4].scatter(*s_ts08.T, s=ss, color='gold', ec='goldenrod', label=f"{ess_ts08:.2f}")
+    for i in range(5):
+        ax[i].set_aspect('equal')
+        ax[i].set_xlabel(r"$\mathregular{\theta_0}$", fontsize=15)
+        ax[i].set_title(titles[i])
+        ax[i].legend()
+    ax[0].set_ylabel(r"$\mathregular{\theta_1}$", fontsize=15)
     plt.tight_layout()
-    plt.legend()
-    plt.savefig("images/thug_hmc_samples_sigma_{}.png".format(
+    plt.legend(fontsize=12)
+    plt.savefig("images/various_algorithms_samples_sigma_{}.png".format(
         str(sigma).replace('.', 'dot')), dpi=300)
     plt.show()
 
